38f702ca614df9a86179f7bb5e884f3e7c7686eb
LadyK-t <67769472+LadyK-t@users.noreply.github.com>
2020-11-10 13:26:42 -0500

    holia

---
+++ ProyectoInformatica/src/proyectoinformatica/ProyectoInformatica.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package proyectoinformatica;
+
+import vista.VstaEdicioDatosAdmin;
+import vista.VstaGraficoDiarioAdmin;
+import vista.VstaInicioAdmin;
+import vista.VstaListarMenuAdmin;
+import vista.VstaModificarComidaAdmin;
+import vista.VstaRanquinSemanalAdmin;
+import vista.VstaRecuperarContrasenaAdmin;
+import vista.VstaRedireccionCorreoAdmin;
+import vista.VstaRegistrarComidaAdmin;
+
+/**
+ *
+ * @author ACER
+ */
+public class ProyectoInformatica {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       /* VstaEdicioDatosAdmin vtnMenuPrincipal= new VstaEdicioDatosAdmin();
+        vtnMenuPrincipal.setVisible(true);
+        VstaGraficoDiarioAdmin vtnMenuPrincipal1= new VstaGraficoDiarioAdmin();
+        vtnMenuPrincipal1.setVisible(true);*/
+        VstaInicioAdmin vtnMenuPrincipal2= new VstaInicioAdmin();
+        vtnMenuPrincipal2.setVisible(true);/*
+        VstaListarMenuAdmin vtnMenuPrincipal3= new VstaListarMenuAdmin();
+        vtnMenuPrincipal3.setVisible(true);
+        VstaModificarComidaAdmin vtnMenuPrincipal4= new VstaModificarComidaAdmin();
+        vtnMenuPrincipal4.setVisible(true);
+        VstaRanquinSemanalAdmin vtnMenuPrincipal5= new VstaRanquinSemanalAdmin();
+        vtnMenuPrincipal5.setVisible(true);
+        VstaRecuperarContrasenaAdmin vtnMenuPrincipal6= new VstaRecuperarContrasenaAdmin();
+        vtnMenuPrincipal6.setVisible(true);
+        VstaRedireccionCorreoAdmin vtnMenuPrincipal7= new VstaRedireccionCorreoAdmin();
+        vtnMenuPrincipal7.setVisible(true);
+        VstaRegistrarComidaAdmin vtnMenuPrincipal8= new VstaRegistrarComidaAdmin();
+        vtnMenuPrincipal8.setVisible(true);*/
+    }
+    
+}
